dfb4181f2e09366587ad283605fe8c9fb0a74fec
Zellars <143442920+Zellars@users.noreply.github.com>
2023-09-11 14:28:24 -0400

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,8 @@
+#FIXME
+current_balance = 25 #underscores are okay!
+previous_balance = 50 #no spaces
+account4 = "Daffy Duck" #numbers in variables need to come after something
+sum_42 = 20 + 22 #variable can be a number but not just a number
+Total_SUM = 2000 #cannot use special symbols
+
+print(current_balance)
